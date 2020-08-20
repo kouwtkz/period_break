ee8d801f7330237957256e14589071ee9110020b
コウラン <38083825+coulank@users.noreply.github.com>
2020-08-20 19:20:35 +0900

    「。。」というような書き方に対応しました

--- content.js
+++ content.js
@@ -8,6 +8,6 @@ function periodBreak() {
     var n = document.getSelection().baseNode;
     n.parentElement.style.whiteSpace = "pre-wrap";
     n.data = n.data
-        .replace(/(。|[.!?！？]\s+)(\n|)/g, function(m, p1, p2){return p1+(p2===''?"\n":p2);})
+        .replace(/([。]+|[.!?！？]\s+)(\n|)/g, function(m, p1, p2){return p1+(p2===''?"\n":p2);})
         .replace(/^\s+/, " ");
 }
