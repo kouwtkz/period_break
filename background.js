afdd7e791c94d645ae26dd7752cb103f63826df2
Kou Watakaze <kouran-22-b@hotmail.co.jp>
2022-03-25 19:06:24 +0900

    マニフェスト変更の仮置

--- background.js
+++ background.js
@@ -1,17 +1,24 @@
 //コンテキストメニューに拡張のメニューを追加
+const activeOption = {
+    active: true,
+    currentWindow: true,
+};
+function sendCurMessage(message, url_re = null, response = null) {
+    if (!response) response = () => {};
+    chrome.tabs.query(activeOption, (tabs) => {
+        var tab = tabs[0];
+        if (tab.url && (!url_re || tab.url.match(url_re))) {
+            chrome.tabs.sendMessage(tabs[0].id, message, response);
+        } else {
+            response(null);
+        }
+    });
+}
 chrome.contextMenus.create({
-    id: 'periodBreakSelection',
-    title: '句読点の次を改行する',
-    contexts: ['selection'], //選択しているときのみメニューに表示される
-    onclick: function(info, tab) {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                "periodBreakAction"
-            );
-        });
-    }
+    id: "periodBreakSelection",
+    title: "句読点の次を改行する",
+    contexts: ["selection", "editable"], //選択しているときのみメニューに表示される
+    onclick: (info, tab) => {
+        sendCurMessage("periodBreakAction");
+    },
 });
--- content.js
+++ content.js
@@ -1,13 +1,22 @@
-chrome.runtime.onMessage.addListener(function(request){
-    if (request == "periodBreakAction") {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if (message == "periodBreakAction") {
         periodBreak();
     }
-    return true;
+    sendResponse();
+    return;
 });
+var replacePeriodBreak = (n)=>{
+    return n.replace(/([。]+|[.!?！？]\s+)(\n|)/g, function(m, p1, p2){return p1+(p2===''?"\n":p2);})
+    .replace(/^\s+/, " ");
+};
 function periodBreak() {
-    var n = document.getSelection().baseNode;
-    n.parentElement.style.whiteSpace = "pre-wrap";
-    n.data = n.data
-        .replace(/([。]+|[.!?！？]\s+)(\n|)/g, function(m, p1, p2){return p1+(p2===''?"\n":p2);})
-        .replace(/^\s+/, " ");
+    var n = null, textareaMode = (document.activeElement.tagName === 'TEXTAREA');
+    if (textareaMode) {
+        n = document.activeElement;
+        n.value = replacePeriodBreak(n.value);
+    } else {
+        n = getSelection().baseNode;
+        n.parentElement.style.whiteSpace = "pre-wrap";
+        n.data = replacePeriodBreak(n.data);
+    }
 }
